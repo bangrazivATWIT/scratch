d21042218bfdc2d99906766830f0943eefdcac08
Vincent <Vincent@DESKTOP-J13JLS7>
2020-09-30 11:33:59 -0400

    Grade ex

--- Hello World/src/HW.java
+++ Hello World/src/HW.java
@@ -1,26 +1,41 @@
 import java.util.Scanner;
 
+/**
+ * 
+ * @author Vincent
+ *
+ */
 public class HW {
 
 	public static void main(String[] args) {
-		int in;
-		int rem;
+		double grade;
 		
 		Scanner input = new Scanner(System.in);
 		
-		System.out.print("Enter the value to check: ");
-		in = input.nextInt();	
+		System.out.print("Enter the student's grade: ");
+		grade = input.nextInt();	
 
-		rem = in % 2;
 		
-		if (rem == 0) {
-			System.out.printf("The value is even.");
+		if (grade >= 90) {
+			System.out.printf("A");
 		}
 		
-		else {
-			System.out.printf("The value is odd.");
+		else if (grade >= 80 && grade <90) {
+			System.out.printf("B");
 		}
-
+		
+		else if (grade >= 70 && grade <80) {
+			System.out.printf("C");
+		}	
+		
+		else if (grade >= 60 && grade <70) {
+			System.out.printf("D");
+		}
+		
+		else if (grade <60) {
+			System.out.printf("F");
+		}
+		
 	}
 
 }
