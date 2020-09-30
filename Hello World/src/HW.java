c81b9d2c2d47e6a632761a61db7425fb879e64a1
Vincent <Vincent@DESKTOP-J13JLS7>
2020-09-30 12:18:23 -0400

    paren fix

--- Hello World/src/HW.java
+++ Hello World/src/HW.java
@@ -13,16 +13,16 @@ public class HW {
 		System.out.print("Enter the student's second grade: ");
 		grade2 = input.nextInt();
 		
-		if (grade1 < 0 || grade2 < 0) {
+		if ((grade1 < 0) || (grade2 < 0)) {
 			System.out.printf("Please enter positive numbers");
 			System.exit(0);
 		}
 		
-		else if (grade1 >= 90 && grade2 >= 90) {
+		else if ((grade1 >= 90) && (grade2 >= 90)) {
 			System.out.printf("Way to go!");
 		}
 		
-		else if (grade1 < 70 || grade2 < 70) {
+		else if ((grade1 < 70) || (grade2 < 70)) {
 			System.out.printf("Study more!");
 		}
 		
