144746d547fb730ab4c786c12c677e06e45a1d30
Vincent <Vincent@DESKTOP-J13JLS7>
2020-10-21 11:37:17 -0400

    for loop ex

--- Hello World/src/HW.java
+++ Hello World/src/HW.java
@@ -3,15 +3,9 @@ import java.util.Scanner;
 public class HW {
 
 	public static void main(String[] args) {
-		@SuppressWarnings("resource")
-		Scanner input = new Scanner(System.in);
-		
-		int n;
-		
-		do {
-			System.out.print("Enter a positive number: ");
-			n = input.nextInt();
-			
-		} while (n<1 || n>100);
+		int count;
+		for (count=100; count <= 200; count ++) {
+			System.out.println(count);
+		}
 	}
 }
