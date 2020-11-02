8ac4834a9df0c3d7f072773e9e260c24953e3d66
Vincent <Vincent@DESKTOP-J13JLS7>
2020-11-02 11:42:27 -0500

    Public vars

--- Hello World/src/HW.java
+++ Hello World/src/HW.java
@@ -1,35 +1,47 @@
 import java.util.Scanner;
 
 public class HW {
-
+	
+	static final int C = 299792458;
+	
 	public static void main(String[] args) {
-		double l, w, a;
+		double E, M;
+		E = Energy();
+		M = Mass();
 		
-		//Initialize input from keyboard
-		Scanner input = new Scanner(System.in);
-		
-		
-		//Print request to user and get responses
-		System.out.print("Enter the length, then the width: ");
-		l = input.nextDouble();
-		w = input.nextDouble();
-		
-		if (l < 0) {
-			System.out.println("The length must be at least 0!");
-			System.exit(0);
-		}
-		
-		if (w < 0) {
-			System.out.println("The length must be at least 0!");
-			System.exit(0);
-		}
-		
-		a = solveArea(l, w);
-		
-		System.out.printf("The area is %.3f%n", a);		
+		System.out.printf("The energy is %f%n", E);
+		System.out.printf("The mass is %f%n", M);
 	}
 	
-	public static double solveArea(double l, double w) {
-		return(l*w);
+	public static double Energy(){
+		double M,E;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter mass: ");
+		M = input.nextDouble();
+		
+		if (M < 0) {
+			System.out.println("The mass must be at least 0!");
+			System.exit(0);
+		}
+		
+		E = M * C * C;
+		return E;
+	}
+	
+	public static double Mass(){
+		double M,E;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter energy: ");
+		E = input.nextDouble();
+		
+		if (E < 0) {
+			System.out.println("The energy must be at least 0!");
+			System.exit(0);
+		}
+		
+		M = E / (C * C);
+		return M;
 	}
 }
